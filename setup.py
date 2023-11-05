b5e2474d909467dcc1b06de1434386aa1d386048
Ruturaj Chavan <ruturajchavan1265@gmail.com>
2023-11-05 12:39:31 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.0.1',
     author='Ruturaj Chavan',
     author_email='ruturajchavan1265@gmail.com',
+    install_requires = ['scikit-learn','pandas','numpy'],
     packages=find_packages()
 )
