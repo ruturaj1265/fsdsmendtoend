6698e2a0cef8cc77e61f2d35789904fdb25f9f59
Ruturaj Chavan <ruturajchavan1265@gmail.com>
2023-11-05 12:33:38 +0530

    updated file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='Ruturaj Chavan',
+    author_email='ruturajchavan1265@gmail.com',
+    packages=find_packages()
+)
